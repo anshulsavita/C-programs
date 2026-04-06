e2ca453574a525717fccad9a792f0f559a5523ec
Anshul savita <anshulsavita370@gmail.com>
2026-04-06 21:56:47 +0530

    add lecture notes of strings in lecture47.c file

--- lecture47.c
+++ lecture47.c
@@ -1,10 +1,43 @@
 #include<stdio.h>
 void main(){
-    //  ****Strings***
+    /* ****Strings***
+    - group of characters
+    Ex 
+        Narayan,Gwalior
+    - String always end with special character Null (ascii value is 0) or \0
+
+    - String constant always comes with double quotes.
+        "Ramayan"
+
+    - String constant always return base address.
+    Ex
+        char x[]="Ramayan";
+        int i;
+        printf("%c",x[1]);  //a
+
+        printf("\n%d",x[1]); //97
+    
+        printf("\n%c",x[1]-32); //A
+        printf("\n%c",x[1]-1);  //b
+
+        // %s read a string from begin address to null pointer.
+    printf("%s",x); //Ramayan
+
+    for(i=0;x[i];i++){
+        printf("%c",x[i]); //Ramayan
+    }
+
+    // ***for taking string input.
+    char x[30];
+    printf("Enter string:");
+    // ****scanf("%s",x); will not allow space between a string.
+    gets(x);
+
+    printf("%s",x); */
+
     // Question --> print length of a string.
     // char x[30],c=0,i;
     // printf("Enter string:");
-    // ****scanf("%s",x); will not allow space between a string.
     // gets(x);
     // for(i=0;x[i];i++){
     //     c++;
@@ -18,15 +51,19 @@ void main(){
     // for(i=0;x[i];i++);
     // printf("%d",i);
 
-    // char x[30],c=0,i;
-    // printf("Enter string:");
-    // gets(x);
-    // for(i=0;x[i];i++){
-    //     if(x[i]=='a' || x[i]=='A'){
-    //         c++;
-    //     }
-    // }
-    // printf("%d",c);
+    // Question--> count how many times "(a) or (A)" comes in a string.
+    char x[30],c=0,i;
+    printf("Enter string:");
+    gets(x);
+    for(i=0;x[i];i++){
+        if(x[i]=='a' || x[i]=='A'){
+            c++;
+        }
+    }
+    printf("%d",c);
 
+    // Quiz--> write a c program to count how many upper case, lowercase, digits, spaces and special characters are present in a string.
+
+    // Quiz--> Write a c program to count how many vowels and cosonents present in a string.
     
 }
