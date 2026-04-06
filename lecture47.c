71d87094d20a24507f13fbfcc317c1dbdadb0883
Anshul savita <anshulsavita370@gmail.com>
2026-04-06 19:47:27 +0530

    add string class

---
+++ lecture47.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main(){
+    //  ****Strings***
+    // Question --> print length of a string.
+    // char x[30],c=0,i;
+    // printf("Enter string:");
+    // ****scanf("%s",x); will not allow space between a string.
+    // gets(x);
+    // for(i=0;x[i];i++){
+    //     c++;
+    // }
+    // printf("%d",c);
+
+    // ******or****
+    // char x[30],c=0,i;
+    // printf("Enter string:");
+    // gets(x);
+    // for(i=0;x[i];i++);
+    // printf("%d",i);
+
+    // char x[30],c=0,i;
+    // printf("Enter string:");
+    // gets(x);
+    // for(i=0;x[i];i++){
+    //     if(x[i]=='a' || x[i]=='A'){
+    //         c++;
+    //     }
+    // }
+    // printf("%d",c);
+
+    
+}
