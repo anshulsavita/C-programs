504759dc8dff8654c77a8df4ee1e10fb2155f827
Anshul savita <anshulsavita370@gmail.com>
2026-04-16 15:13:02 +0530

    Solve quiz question of strings

--- lecture51.c
+++ lecture51.c
@@ -39,21 +39,71 @@ void main(){
 
     // ****Multi dimension or two dimension**
     // Suntax--> <type> <var>[rows][cols]
-    int x[3][5];
-    int i,j,s=0;
-    for(i=0;i<=2;i++){
-        for(j=0;j<=4;j++){
-            printf("Enter value @ %d,%d:",i,j);
-            scanf("%d",&x[i][j]);
-        }
-    }
-    for(i=0;i<=2;i++){
-        s=0;
-        for(j=0;j<=4;j++){
-            printf("%d ",x[i][j]);
-            s=s+x[i][j];
-        }
-        printf(" %d \n",s);
-    }
+    // Question--> find the sum of all elements of each row seperately. 
+    // int x[3][5];
+    // int i,j,s=0;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=4;j++){
+    //         printf("Enter value @ %d,%d:",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     s=0;
+    //     for(j=0;j<=4;j++){
+    //         printf("%d ",x[i][j]);
+    //         s=s+x[i][j];
+    //     }
+    //     printf(" %d \n",s);
+    // }
 
+    // *****Quiz-->input acts and find output a@c@t@s.
+    // char x[30];
+    // int i,j,t,l;
+    // printf("Enter a string:");
+    // gets(x);
+    // l=strlen(x); // for find length
+
+    // for(i=1;x[i];i++){
+    //     for(j=l;j>=i;j--){
+    //         x[j+1]=x[j];
+    //         t=j;
+    //     }
+    //     l++;
+    //     i++;
+    //     x[t]='@';
+    // }
+    // printf("%s",x);    
+
+    // *****Quiz-->input AJAY and find output JAYA.
+    // char x[30];
+    // int i,t;
+    // printf("Enter a string:");
+    // gets(x);
+    // for(i=0;x[i];i++){
+    //     t=x[i];
+    //     x[i]=x[i+1];
+    //     x[i+1]=t;
+    //     i++;
+    // }
+    // printf("%s",x);
+    
+    // *****Quiz-->find the sum of all elements of each coloums.
+    // int x[3][3];
+    // int i,j,s=0;
+    // for(i=0;i<=2;i++){
+    //     for(j=0;j<=2;j++){
+    //         printf("Enter value @ %d,%d:",i,j);
+    //         scanf("%d",&x[i][j]);
+    //     }
+    // }
+    // for(i=0;i<=2;i++){
+    //     s=0;
+    //     for(j=0;j<=2;j++){
+    //         printf("%d ",x[i][j]);
+    //         s=s+x[j][i];
+    //     }
+    //     printf("%d",s);
+    //     printf("\n");
+    // }
 }
