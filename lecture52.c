1cb541f351a0d21bf8dd442c383b7c5f096eac27
Anshul savita <anshulsavita370@gmail.com>
2026-04-18 18:21:27 +0530

    fix name of lecture 52 on git

--- lecture52.c
+++ lecture52.c
@@ -100,7 +100,7 @@ void main(){
     }
     for(i=0;i<=2;i++){
         for(j=0;j<=2;j++){
-            printf("%d ",z[i][j]);
+            printf(" %d",z[i][j]);
         }
         printf("\n");
     }
