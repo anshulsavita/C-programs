2dd0a32eae95af348742c18fdd13cf6206883162
Anshul savita <anshulsavita370@gmail.com>
2026-05-12 14:59:41 +0530

    Added lecture 60 where recursion and nexted function has been covered and also quiz questions of functions

--- array.h
+++ array.h
@@ -1,5 +1,5 @@
 
-// ***it is a made up header file used in lecture 59
+// ***it is a made up header file used in lecture 59 and lecture 60.
 void getArray(int *x,int n){
     int i;
     for(i=0;i<n;i++){
@@ -14,4 +14,38 @@ void putArray(int *x,int n){
         printf("%d,",x[i]);
     }
     printf("\n");
+}
+void sort(int *x,int n){
+    int i,j,t;
+    for(i=0;i<n;i++){
+        for(j=0;j<n-i;j++){
+            if(x[j]>x[j+1]){
+                t=x[j];
+                x[j]=x[j+1];
+                x[j+1]=t;
+            }
+        }
+    }
+}
+void insert(int *x,int n,int no, int pos){
+    int j;
+    for(j=n-1;j>=pos;j--){
+        x[j+1]=x[j];
+    }
+    x[pos]=no;
+}
+void del(int *x,int n,int pos){
+    int i,j;
+    for(i=pos;i<n;i++){
+        x[i]=x[i+1];
+    }
+}
+void merge(int *x,int n1, int *y,int n2, int *z){
+    int i,j;
+    for(i=0;i<n1;i++){
+        z[i]=x[i];
+    }
+    for(i=0;i<n2;i++){
+        z[i+n1]=y[i];
+    }
 }
---
+++ lecture60.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include"array.h"
+// void main(){
+// *****question-->sort(x,10)
+    // int x[10];
+    // getArray(x,10);
+    // printf("Unsorted values:\n");
+    // putArray(x,10);
+    // sort(x,10);
+    // printf("Sorted values:\n");
+    // putArray(x,10);
+
+// *****quiz-->insert(x,10,100,5)
+    // int x[10];
+    // getArray(x,9);
+    // insert(x,10,5,3);
+    // putArray(x,10);
+    
+// *****quiz-->del(x,10,5)
+    // int x[5];
+    // getArray(x,5);
+    // del(x,5,3);
+    // putArray(x,4);
+
+// *****quiz-->merge(x,10,y,5,z)
+    // int x[5],y[5],z[10];
+    // getArray(x,5);
+    // getArray(y,5);
+    // merge(x,5,y,5,z);
+    // putArray(z,10);
+// }
+
+/* ****Recursion-->
+ - function itself call a function knows as recursion.
+ - recursion must be control by some condition otherwise it will crash the program, it works on stack.
+ - recursion used stack to remmember the process. while iteration never use stack.
+ - recursion is used for backtracking.
+
+***backtracking--> when we go in a dense forsest we can easily forget wright direction,
+so we mark checkpoints using flags and when we start to return, we take off the last flag first and then remainings.
+*/
+
+// ****backtracking example-->
+// void call(int n){
+//     if(n>=1){
+//         printf("Hello %d\n",n);
+//         call(n-1);
+//     }
+//     printf("Hi %d\n",n);
+// }
+// void main(){
+//     call(5);
+// }
+
+// ***nexted functions
+void usa(){
+    printf("In USA\n");
+}
+void lanka(){
+    printf("In lanka\n");
+    usa();
+    printf("Again in lanka\n");
+}
+void india(){
+    printf("In India\n");
+    lanka();
+    printf("Again in India\n");
+}
+void main(){
+    printf("Start\n");
+    india();
+    printf("End\n");
+}
+
